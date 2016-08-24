1b5f273de3426fd88d2826ff9a95a04a88775932
Bryan He <bryanhe@stanford.edu>
2016-08-24 15:50:53 -0700

    Added all non-categorical factors

--- numbskull/inference.py
+++ numbskull/inference.py
@@ -49,19 +49,78 @@ def potential(var_samp, value, var_copy, weight_copy, weight, variable, factor, 
 
 @jit(nopython=True,cache=True,nogil=True)
 def eval_factor(factor_id, var_samp, value, var_copy, weight, variable, factor, fstart, fmap, vstart, vmap, equalPred, Z, count, var_value, weight_value):
-    if factor[factor_id]["factorFunction"] == 3: # FUNC_EQUAL
+    if factor[factor_id]["factorFunction"] == 0: # FUNC_IMPLY_NATURAL
+        for l in range(fstart[factor_id], fstart[factor_id + 1] - 1):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == 0:
+                # Early return if body is not satisfied
+                return 0
+
+        # If this point is reached, body must be true
+        head = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[factor_id + 1] - 1]
+        if head:
+            return 1
+        return -1
+    elif factor[factor_id]["factorFunction"] == 1: # FUNC_OR
+        for l in range(fstart[factor_id], fstart[factor_id + 1]):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == 1:
+                return 1
+        return -1
+    elif factor[factor_id]["factorFunction"] == 3: # FUNC_EQUAL
         v = value if (fmap[fstart[factor_id]] == var_samp) else var_value[var_copy][fmap[fstart[factor_id]]]
         for l in range(fstart[factor_id] + 1, fstart[factor_id + 1]):
             w = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
             if v != w:
                 return -1
         return 1
-    elif factor[factor_id]["factorFunction"] == 4: # FUNC_ISTRUE
+    elif factor[factor_id]["factorFunction"] == 2 \
+      or factor[factor_id]["factorFunction"] == 4: # FUNC_AND or FUNC_ISTRUE
         for l in range(fstart[factor_id], fstart[factor_id + 1]):
             v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
             if v == 0:
                 return -1
         return 1
+    elif factor[factor_id]["factorFunction"] == 7: # FUNC_LINEAR
+        res = 0
+        head = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[factor_id + 1] - 1]
+        for l in range(fstart[factor_id], fstart[factor_id + 1] - 1):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == head:
+                res += 1
+        # This does not match Dimmwitted, but matches the eq in the paper
+        return res
+    elif factor[factor_id]["factorFunction"] == 8: # FUNC_RATIO
+        res = 1
+        head = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[factor_id + 1] - 1]
+        for l in range(fstart[factor_id], fstart[factor_id + 1] - 1):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == head:
+                res += 1
+        # This does not match Dimmwitted, but matches the eq in the paper
+        return math.log(res) # TODO: use log2?
+    elif factor[factor_id]["factorFunction"] == 9: # FUNC_LOGICAL
+        head = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[factor_id + 1] - 1]
+        for l in range(fstart[factor_id], fstart[factor_id + 1] - 1):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == head:
+                return 1
+        return 0
+    elif factor[factor_id]["factorFunction"] == 12: # FUNC_AND_CATEGORICAL
+        # TODO
+        pass
+    elif factor[factor_id]["factorFunction"] == 13: # FUNC_IMPLY_MLN
+        for l in range(fstart[factor_id], fstart[factor_id + 1] - 1):
+            v = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[l]]
+            if v == 0:
+                # Early return if body is not satisfied
+                return 1
+
+        # If this point is reached, body must be true
+        head = value if (fmap[l] == var_samp) else var_value[var_copy][fmap[factor_id + 1] - 1]
+        if head:
+            return 1
+        return 0
     else: # FUNC_UNDEFINED
         print("Error: Factor Function", factor[factor_id]["factorFunction"], "( used in factor", factor_id, ") is not implemented.")
         raise NotImplementedError("Factor function is not implemented.")
