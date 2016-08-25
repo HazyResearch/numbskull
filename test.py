92178cb255d8c8d4d9f26eb9184a8756f8bdd7d2
Bryan He <bryanhe@stanford.edu>
2016-08-24 20:00:19 -0700

    Add very basic test (just runs code)

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from numbskull import numbskull
+
+args = ['../../sampler/ising', '-l','100','-i', '100', '-t', '1','-s','0.001']
+ns = numbskull.main(args)
+ns.learning()
+ns.inference()
+print(ns.factorGraphs[0].count)
+
