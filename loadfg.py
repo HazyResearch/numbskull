9097c3f70a2c35dd641f4f9e43e295bdfb8f961e
Bryan He <bryanhe@stanford.edu>
2016-08-25 22:30:28 -0700

    Loop over factor functions

--- loadfg.py
+++ loadfg.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python
 
-from numbskull import numbskull
+import numbskull
 from numbskull.numbskulltypes import *
 import numpy as np
 
-weight = np.empty(1, Weight)
-variable = np.empty(2, Variable)
-factor = np.empty(1, Factor)
-equalPredicate = np.empty(1, np.int32)
-fstart = np.empty(1, np.int64)
-fmap = np.empty(1, np.int64)
-edges = 2
+for (key, value) in numbskull.inference.FACTORS.iteritems():
 
-weight[0]["isFixed"] = True
-weight[0]["initialValue"] = 1
+    variables = 2
 
-variable[0]["isEvidence"] = 0
-variable[0]["initialValue"] = 0
-variable[0]["dataType"] = 0
-variable[0]["cardinality"] = 2
+    weight = np.empty(1, Weight)
+    variable = np.empty(variables, Variable)
+    factor = np.empty(1, Factor)
+    equalPredicate = np.empty(variables, np.int32)  # TODO: for binary?
+    fstart = np.empty(2, np.int64)
+    fmap = np.empty(variables, np.int64)
 
-variable[1]["isEvidence"] = 0
-variable[1]["initialValue"] = 0
-variable[1]["dataType"] = 0
-variable[1]["cardinality"] = 2
+    edges = variables
 
-factor[0]["factorFunction"] = 1
-factor[0]["weightId"] = 0
-factor[0]["featureValue"] = 0
+    weight[0]["isFixed"] = True
+    weight[0]["initialValue"] = 1
 
-ns = numbskull.NumbSkull(n_inference_epoch=100, n_learning_epoch=100)
-ns.loadFactorGraph(weight, variable, factor, fstart,
-                   fmap, equalPredicate, edges)
+    variable[0]["isEvidence"] = 0
+    variable[0]["initialValue"] = 0
+    variable[0]["dataType"] = 0
+    variable[0]["cardinality"] = 2
 
-ns.learning()
-ns.inference()
-print(ns.factorGraphs[0].count)
+    variable[1]["isEvidence"] = 0
+    variable[1]["initialValue"] = 0
+    variable[1]["dataType"] = 0
+    variable[1]["cardinality"] = 2
+
+    factor[0]["factorFunction"] = value
+    factor[0]["weightId"] = 0
+    factor[0]["featureValue"] = 0
+
+    fstart[0] = 0
+    fstart[1] = variables
+
+    for i in range(variables):
+        fmap[i] = i
+
+    ns = numbskull.NumbSkull(n_inference_epoch=100, n_learning_epoch=100)
+    ns.loadFactorGraph(weight, variable, factor, fstart,
+                       fmap, equalPredicate, edges)
+
+    ns.learning()
+    ns.inference()
+    print(ns.factorGraphs[0].count)
