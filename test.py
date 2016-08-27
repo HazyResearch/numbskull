dbe78452860b32b4d30aafa288b4c40d66249b09
Bryan He <bryanhe@stanford.edu>
2016-08-27 00:05:02 -0700

    pep8

--- test.py
+++ test.py
@@ -2,7 +2,14 @@
 
 from numbskull import numbskull
 
-args = ['test', '-l', '100', '-i', '100', '-t', '10', '-s', '0.01', '--regularization', '2', '-r', '0.1', '--quiet']
+args = ['test',
+        '-l', '100',
+        '-i', '100',
+        '-t', '10',
+        '-s', '0.01',
+        '--regularization', '2',
+        '-r', '0.1',
+        '--quiet']
 ns = numbskull.main(args)
 ns.learning()
 ns.inference()
