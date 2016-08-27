4a914c070fbbb95a2dae5de4a0ba760c2bd8482e
Bryan He <bryanhe@stanford.edu>
2016-08-27 00:04:29 -0700

    make test quiet

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from numbskull import numbskull
 
-args = ['test', '-l', '100', '-i', '100', '-t', '10', '-s', '0.01', '--regularization', '2', '-r', '0.1']
+args = ['test', '-l', '100', '-i', '100', '-t', '10', '-s', '0.01', '--regularization', '2', '-r', '0.1', '--quiet']
 ns = numbskull.main(args)
 ns.learning()
 ns.inference()
