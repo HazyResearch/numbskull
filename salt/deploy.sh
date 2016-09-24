2daaf81ed89a731361a86d470f5da252f0af0ee9
thodrek <thodrek@stanford.edu>
2016-09-24 09:16:04 -0700

    removing salt from master. Under development

--- salt/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp numbskull_minion.py /tmp/salt/srv/salt/_engines/
-cp -r ../numbskull /tmp/salt/srv/salt/_modules/
-salt "*" saltutil.sync_engines
-salt "*" saltutil.sync_modules
-#salt "*" service.restart salt-minion
--- salt/start_minions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `seq 2 10`
-do
-    ssh raiders${i} "salt-minion -l debug &"
-done
-
