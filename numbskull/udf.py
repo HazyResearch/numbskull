4c447942238dcc9a6aeb34ab1338373e5d357e8d
Paroma Varma <varma.paroma@gmail.com>
2017-05-03 14:47:41 -0700

    UDFs for visual genome

--- numbskull/udf.py
+++ numbskull/udf.py
@@ -16,7 +16,9 @@ UDF_CARDINALITY = {
     "TOY_AND":    2,
     "TOY_DIRECT": 1,
     "TOY_DIRECT_ABSTAIN": 1,
-    "TOY_DIRECT_SINGLE": 1
+    "TOY_DIRECT_SINGLE": 1,
+    "VG_12": 2,
+    "VG_POS_SIZE_NUM": 3
 }
 
 # Automatically select a unique index for each UDF (no modification needed)
@@ -35,7 +37,8 @@ for (key, value) in UDF_INDEX.items():
 # USER: Specify the list of UDFs that are used in a single model.
 UDF_USAGE = {
     "TOY": [TOY_AND, TOY_DIRECT, TOY_DIRECT],
-    "DDSM": [TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_SINGLE, TOY_DIRECT_ABSTAIN]
+    "DDSM": [TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_ABSTAIN, TOY_DIRECT_SINGLE, TOY_DIRECT_ABSTAIN],
+    "VG": [VG_12, VG_12, VG_POS_SIZE_NUM, VG_POS_SIZE_NUM, VG_POS_SIZE_NUM]
 }
 
 # USER: There are not modifications necessary here. However, the value
@@ -85,15 +88,46 @@ def udf(udf_index, var_samp, value, var_copy, var_value, fmap, ftv_start):
         v1 = value               if (fmap[ftv_start + 0]["vid"] == var_samp) \
             else var_value[var_copy][fmap[ftv_start + 0]["vid"]]
         return 2 * v1 - 1
+    
+    #DDSM Labeling Functions
     elif udf_index == TOY_DIRECT_ABSTAIN:
         v1 = value               if (fmap[ftv_start + 0]["vid"] == var_samp) \
             else var_value[var_copy][fmap[ftv_start + 0]["vid"]]
         return v1-1
     elif udf_index == TOY_DIRECT_SINGLE:
-        #print("Working")
         v1 = value               if (fmap[ftv_start + 0]["vid"] == var_samp) \
             else var_value[var_copy][fmap[ftv_start + 0]["vid"]]
         return v1-1
+    
+    #Visual Genome Labeling Functions
+    elif udf_index == VG_12:
+        v1 = value               if (fmap[ftv_start + 0]["vid"] == var_samp) \
+            else var_value[var_copy][fmap[ftv_start + 0]["vid"]]
+        v2 = value               if (fmap[ftv_start + 1]["vid"] == var_samp) \
+            else var_value[var_copy][fmap[ftv_start + 1]["vid"]]
+        
+        if v1 == 1:
+            if v2 == 1:
+                return 1
+            else:
+                return -1
+        return 0
+    elif udf_index == VG_POS_SIZE_NUM:
+        v1 = value               if (fmap[ftv_start + 0]["vid"] == var_samp) \
+            else var_value[var_copy][fmap[ftv_start + 0]["vid"]]
+        v2 = value               if (fmap[ftv_start + 1]["vid"] == var_samp) \
+            else var_value[var_copy][fmap[ftv_start + 1]["vid"]]
+        v3 = value               if (fmap[ftv_start + 2]["vid"] == var_samp) \
+            else var_value[var_copy][fmap[ftv_start + 2]["vid"]]
+        
+        if v1 == 1:
+            if v2 == 1:
+                if v3 == 2:
+                    return 1
+            else:
+                if v3 == 0:
+                    return -1
+        return 0
     else:
         print("Error: UDF", udf_index,
               "is not implemented.")
