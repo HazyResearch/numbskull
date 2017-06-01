d60f9c6e455eba3c7964f068a52d82fea4a2aeec
Bryan He <bryanhe@stanford.edu>
2017-05-31 17:07:57 -0700

    specifying dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version=__version__,
     description='sample away',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['futures'],
     entry_points={
         'console_scripts': [
             'numbskull = numbskull.numbskull:main',
